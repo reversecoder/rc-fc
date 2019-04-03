aa80daf94d946df0a9887a76105e67a25ad01ae9
Md. Rashadul Alam <rashedul.alam@bjitgroup.com>
2019-04-03 16:38:24 +0600

    rc-fs: release 1.0.1

--- app/src/main/java/com/rc/facecase/activity/HomeActivity.java
+++ app/src/main/java/com/rc/facecase/activity/HomeActivity.java
@@ -1,213 +1,213 @@
-package com.rc.facecase.activity;
-
-import android.content.Context;
-import android.content.Intent;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.Toast;
-
-import com.rc.facecase.R;
-import com.rc.facecase.base.BaseActivity;
-import com.rc.facecase.model.AppUser;
-import com.rc.facecase.model.ParamsAppUser;
-import com.rc.facecase.retrofit.APIClient;
-import com.rc.facecase.retrofit.APIInterface;
-import com.rc.facecase.retrofit.APIResponse;
-import com.rc.facecase.util.AllConstants;
-import com.rc.facecase.util.AppUtil;
-import com.rc.facecase.util.Logger;
-import com.reversecoder.library.event.OnSingleClickListener;
-import com.reversecoder.library.network.NetworkManager;
-import com.reversecoder.library.storage.SessionManager;
-import com.reversecoder.library.util.AllSettingsManager;
-
-import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Response;
-
-public class HomeActivity extends BaseActivity {
-
-    ImageView ivHome;
-    private AppUser mAppUser;
-
-    //Background task
-    private APIInterface mApiInterface;
-    private RegisterAppUserTask registerAppUserTask;
-
-    @Override
-    public String[] initActivityPermissions() {
-        return new String[]{};
-    }
-
-    @Override
-    public int initActivityLayout() {
-        return R.layout.activity_home_screen;
-
-    }
-
-    @Override
-    public void initStatusBarView() {
-//        requestWindowFeature(Window.FEATURE_NO_TITLE);
-//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-//                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-    }
-
-    @Override
-    public void initNavigationBarView() {
-
-    }
-
-    @Override
-    public void initIntentData(Bundle savedInstanceState, Intent intent) {
-
-    }
-
-    @Override
-    public void initActivityViews() {
-        ivHome = (ImageView) findViewById(R.id.iv_home);
-    }
-
-    @Override
-    public void initActivityViewsData(Bundle savedInstanceState) {
-        mApiInterface = APIClient.getClient(getActivity()).create(APIInterface.class);
-        String deviceUniqueIdentity = SessionManager.getStringSetting(getActivity(), AllConstants.SESSION_DEVICE_IDENTIFIER);
-
-        if (deviceUniqueIdentity == null || deviceUniqueIdentity.equalsIgnoreCase("")){
-            deviceUniqueIdentity = AppUtil.getAppDeviceUniqueIdentifier(getActivity());
-        }
-
-        Logger.d(TAG, TAG + " >>> " + "AppUser(deviceUniqueIdentity): " +deviceUniqueIdentity);
-        //Check internet connection
-        if (!NetworkManager.isConnected(getActivity())) {
-            Toast.makeText(getActivity(), getResources().getString(R.string.toast_network_error), Toast.LENGTH_SHORT).show();
-
-        } else {
-
-            String appUserID = SessionManager.getStringSetting(getActivity(), AllConstants.SESSION_KEY_USER);
-            if (!AllSettingsManager.isNullOrEmpty(appUserID)) {
-                mAppUser = APIResponse.getResponseObject(appUserID, AppUser.class);
-                Logger.d(TAG, TAG + " >>> " + "mAppUser: " + mAppUser.toString());
-            }
-
-            if (mAppUser==null) {
-                //Register app user
-                ParamsAppUser paramAppUser = new ParamsAppUser(deviceUniqueIdentity);
-                registerAppUserTask = new RegisterAppUserTask(getActivity(), paramAppUser);
-                registerAppUserTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
-            }
-        }
-
-
-    }
-
-    @Override
-    public void initActivityActions(Bundle savedInstanceState) {
-        ivHome.setOnClickListener(new OnSingleClickListener() {
-            @Override
-            public void onSingleClick(View view) {
-                Intent iFacePlay=new Intent(getApplicationContext(), FaceCasePlayActivity.class);
-                startActivity(iFacePlay);
-            }
-        });
-    }
-
-    @Override
-    public void initActivityOnResult(int requestCode, int resultCode, Intent data) {
-
-    }
-
-    @Override
-    public void initActivityBackPress() {
-       finish();
-    }
-
-    @Override
-    public void initActivityDestroyTasks() {
-        dismissProgressDialog();
-        if (registerAppUserTask != null && registerAppUserTask.getStatus() == AsyncTask.Status.RUNNING) {
-            registerAppUserTask.cancel(true);
-        }
-    }
-
-    @Override
-    public void initActivityPermissionResult(int requestCode, String[] permissions, int[] grantResults) {
-
-    }
-    /************************
-     * Server communication *
-     ************************/
-    private class RegisterAppUserTask extends AsyncTask<String, Integer, Response> {
-
-        Context mContext;
-        ParamsAppUser mParamAppUser;
-
-        private RegisterAppUserTask(Context context, ParamsAppUser paramAppUser) {
-            mContext = context;
-            mParamAppUser = paramAppUser;
-        }
-
-        @Override
-        protected void onPreExecute() {
-            showProgressDialog();
-        }
-
-        @Override
-        protected Response doInBackground(String... params) {
-            try {
-                Logger.d(TAG, TAG + " >>> " + "AppUser(home-param): " + mParamAppUser.toString());
-                Call<APIResponse<List<AppUser>>> call = mApiInterface.apiAddUser(mParamAppUser);
-
-                Response response = call.execute();
-                Logger.d(TAG, TAG + " >>> " + "response: " + response);
-                if (response.isSuccessful()) {
-                    return response;
-                }
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Response result) {
-            try {
-                dismissProgressDialog();
-
-                if (result != null && result.isSuccessful()) {
-                    Logger.d(TAG, TAG + " >>> " + "APIResponse(DoCreateUser): onResponse-server = " + result.toString());
-                    APIResponse<List<AppUser>> data = (APIResponse<List<AppUser>>) result.body();
-                    Logger.d(TAG, TAG + " >>> " + "data: " + data.toString() + "");
-
-                    if (data != null && data.getStatus().equalsIgnoreCase("1")) {
-                        Logger.d(TAG, TAG + " >>> " + "APIResponse(DoCreateUser()): onResponse-object = " + data.toString());
-
-                            //Save data into session
-                            String jsonAppUser = APIResponse.getResponseString(data.getData().get(0));
-                            Logger.d(TAG, TAG + " >>> " + "APIResponse(DoCreateUser()): app-user = " + jsonAppUser);
-                            Logger.d(TAG, TAG + " >>> " + "AppUser(home-response): " + jsonAppUser);
-                            Log.e("AppUser>>>",data.getData().toString()+">>>");
-                            SessionManager.setStringSetting(getActivity(), AllConstants.SESSION_KEY_USER,  jsonAppUser);
-//                            //Navigate to the home
-//                            Intent intentHome = new Intent(getActivity(), HomeActivity.class);
-//                            startActivity(intentHome);
-//                            finish();
-
-                    } else {
-                        Toast.makeText(getActivity(), getResources().getString(R.string.toast_no_info_found), Toast.LENGTH_SHORT).show();
-                    }
-                } else {
-                    Toast.makeText(getActivity(), getResources().getString(R.string.toast_could_not_retrieve_info), Toast.LENGTH_SHORT).show();
-                }
-            } catch (Exception exception) {
-                exception.printStackTrace();
-            }
-        }
-    }
-
-
-}
+//package com.rc.facecase.activity;
+//
+//import android.content.Context;
+//import android.content.Intent;
+//import android.os.AsyncTask;
+//import android.os.Bundle;
+//import android.util.Log;
+//import android.view.View;
+//import android.widget.ImageView;
+//import android.widget.Toast;
+//
+//import com.rc.facecase.R;
+//import com.rc.facecase.base.BaseActivity;
+//import com.rc.facecase.model.AppUser;
+//import com.rc.facecase.model.ParamsAppUser;
+//import com.rc.facecase.retrofit.APIClient;
+//import com.rc.facecase.retrofit.APIInterface;
+//import com.rc.facecase.retrofit.APIResponse;
+//import com.rc.facecase.util.AllConstants;
+//import com.rc.facecase.util.AppUtil;
+//import com.rc.facecase.util.Logger;
+//import com.reversecoder.library.event.OnSingleClickListener;
+//import com.reversecoder.library.network.NetworkManager;
+//import com.reversecoder.library.storage.SessionManager;
+//import com.reversecoder.library.util.AllSettingsManager;
+//
+//import java.util.List;
+//
+//import retrofit2.Call;
+//import retrofit2.Response;
+//
+//public class HomeActivity extends BaseActivity {
+//
+//    ImageView ivHome;
+//    private AppUser mAppUser;
+//
+//    //Background task
+//    private APIInterface mApiInterface;
+//    private RegisterAppUserTask registerAppUserTask;
+//
+//    @Override
+//    public String[] initActivityPermissions() {
+//        return new String[]{};
+//    }
+//
+//    @Override
+//    public int initActivityLayout() {
+//        return R.layout.activity_home_screen;
+//
+//    }
+//
+//    @Override
+//    public void initStatusBarView() {
+////        requestWindowFeature(Window.FEATURE_NO_TITLE);
+////        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+////                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//    }
+//
+//    @Override
+//    public void initNavigationBarView() {
+//
+//    }
+//
+//    @Override
+//    public void initIntentData(Bundle savedInstanceState, Intent intent) {
+//
+//    }
+//
+//    @Override
+//    public void initActivityViews() {
+//        ivHome = (ImageView) findViewById(R.id.iv_home);
+//    }
+//
+//    @Override
+//    public void initActivityViewsData(Bundle savedInstanceState) {
+//        mApiInterface = APIClient.getClient(getActivity()).create(APIInterface.class);
+//        String deviceUniqueIdentity = SessionManager.getStringSetting(getActivity(), AllConstants.SESSION_DEVICE_IDENTIFIER);
+//
+//        if (deviceUniqueIdentity == null || deviceUniqueIdentity.equalsIgnoreCase("")){
+//            deviceUniqueIdentity = AppUtil.getAppDeviceUniqueIdentifier(getActivity());
+//        }
+//
+//        Logger.d(TAG, TAG + " >>> " + "AppUser(deviceUniqueIdentity): " +deviceUniqueIdentity);
+//        //Check internet connection
+//        if (!NetworkManager.isConnected(getActivity())) {
+//            Toast.makeText(getActivity(), getResources().getString(R.string.toast_network_error), Toast.LENGTH_SHORT).show();
+//
+//        } else {
+//
+//            String appUserID = SessionManager.getStringSetting(getActivity(), AllConstants.SESSION_KEY_USER);
+//            if (!AllSettingsManager.isNullOrEmpty(appUserID)) {
+//                mAppUser = APIResponse.getResponseObject(appUserID, AppUser.class);
+//                Logger.d(TAG, TAG + " >>> " + "mAppUser: " + mAppUser.toString());
+//            }
+//
+//            if (mAppUser==null) {
+//                //Register app user
+//                ParamsAppUser paramAppUser = new ParamsAppUser(deviceUniqueIdentity);
+//                registerAppUserTask = new RegisterAppUserTask(getActivity(), paramAppUser);
+//                registerAppUserTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+//            }
+//        }
+//
+//
+//    }
+//
+//    @Override
+//    public void initActivityActions(Bundle savedInstanceState) {
+//        ivHome.setOnClickListener(new OnSingleClickListener() {
+//            @Override
+//            public void onSingleClick(View view) {
+//                Intent iFacePlay=new Intent(getApplicationContext(), FaceCasePlayActivity.class);
+//                startActivity(iFacePlay);
+//            }
+//        });
+//    }
+//
+//    @Override
+//    public void initActivityOnResult(int requestCode, int resultCode, Intent data) {
+//
+//    }
+//
+//    @Override
+//    public void initActivityBackPress() {
+//       finish();
+//    }
+//
+//    @Override
+//    public void initActivityDestroyTasks() {
+//        dismissProgressDialog();
+//        if (registerAppUserTask != null && registerAppUserTask.getStatus() == AsyncTask.Status.RUNNING) {
+//            registerAppUserTask.cancel(true);
+//        }
+//    }
+//
+//    @Override
+//    public void initActivityPermissionResult(int requestCode, String[] permissions, int[] grantResults) {
+//
+//    }
+//    /************************
+//     * Server communication *
+//     ************************/
+//    private class RegisterAppUserTask extends AsyncTask<String, Integer, Response> {
+//
+//        Context mContext;
+//        ParamsAppUser mParamAppUser;
+//
+//        private RegisterAppUserTask(Context context, ParamsAppUser paramAppUser) {
+//            mContext = context;
+//            mParamAppUser = paramAppUser;
+//        }
+//
+//        @Override
+//        protected void onPreExecute() {
+//            showProgressDialog();
+//        }
+//
+//        @Override
+//        protected Response doInBackground(String... params) {
+//            try {
+//                Logger.d(TAG, TAG + " >>> " + "AppUser(home-param): " + mParamAppUser.toString());
+//                Call<APIResponse<List<AppUser>>> call = mApiInterface.apiAddUser(mParamAppUser);
+//
+//                Response response = call.execute();
+//                Logger.d(TAG, TAG + " >>> " + "response: " + response);
+//                if (response.isSuccessful()) {
+//                    return response;
+//                }
+//            } catch (Exception ex) {
+//                ex.printStackTrace();
+//            }
+//
+//            return null;
+//        }
+//
+//        @Override
+//        protected void onPostExecute(Response result) {
+//            try {
+//                dismissProgressDialog();
+//
+//                if (result != null && result.isSuccessful()) {
+//                    Logger.d(TAG, TAG + " >>> " + "APIResponse(DoCreateUser): onResponse-server = " + result.toString());
+//                    APIResponse<List<AppUser>> data = (APIResponse<List<AppUser>>) result.body();
+//                    Logger.d(TAG, TAG + " >>> " + "data: " + data.toString() + "");
+//
+//                    if (data != null && data.getStatus().equalsIgnoreCase("1")) {
+//                        Logger.d(TAG, TAG + " >>> " + "APIResponse(DoCreateUser()): onResponse-object = " + data.toString());
+//
+//                            //Save data into session
+//                            String jsonAppUser = APIResponse.getResponseString(data.getData().get(0));
+//                            Logger.d(TAG, TAG + " >>> " + "APIResponse(DoCreateUser()): app-user = " + jsonAppUser);
+//                            Logger.d(TAG, TAG + " >>> " + "AppUser(home-response): " + jsonAppUser);
+//                            Log.e("AppUser>>>",data.getData().toString()+">>>");
+//                            SessionManager.setStringSetting(getActivity(), AllConstants.SESSION_KEY_USER,  jsonAppUser);
+////                            //Navigate to the home
+////                            Intent intentHome = new Intent(getActivity(), HomeActivity.class);
+////                            startActivity(intentHome);
+////                            finish();
+//
+//                    } else {
+//                        Toast.makeText(getActivity(), getResources().getString(R.string.toast_no_info_found), Toast.LENGTH_SHORT).show();
+//                    }
+//                } else {
+//                    Toast.makeText(getActivity(), getResources().getString(R.string.toast_could_not_retrieve_info), Toast.LENGTH_SHORT).show();
+//                }
+//            } catch (Exception exception) {
+//                exception.printStackTrace();
+//            }
+//        }
+//    }
+//
+//
+//}
--- app/src/main/java/com/rc/facecase/activity/TestActivity.java
+++ app/src/main/java/com/rc/facecase/activity/TestActivity.java
@@ -1,63 +1,63 @@
-package com.rc.facecase.activity;
-
-import android.graphics.Bitmap;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.ImageView;
-
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.target.SimpleTarget;
-import com.bumptech.glide.request.transition.Transition;
-import com.rc.facecase.R;
-import com.rodolfonavalon.shaperipplelibrary.ShapeRipple;
-import com.rodolfonavalon.shaperipplelibrary.model.Image;
-
-public class TestActivity extends AppCompatActivity {
-
-    private ImageView ivLoading;
-    private ShapeRipple shapeRipple;
-    private String imageUrl = "http://iexpresswholesale.com/faceoff-games/uploads/pictures/pele.jpg";
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_test);
-
-        ivLoading = (ImageView) findViewById(R.id.iv_loading);
-        shapeRipple = (ShapeRipple) findViewById(R.id.shape_ripple);
-        try {
-            // Load
-            Glide.with(TestActivity.this)
-                    .asGif()
-                    .load(R.drawable.gif_loading)
-                    .into(ivLoading);
-
-            Glide
-                    .with(TestActivity.this)
-                    .asBitmap()
-                    .load(imageUrl)
-                    .into(new SimpleTarget<Bitmap>() {
-                        @Override
-                        public void onResourceReady(Bitmap bitmap, Transition<? super Bitmap> transition) {
-                            //you can use loaded bitmap here
-                            shapeRipple.setVisibility(View.VISIBLE);
-                            ivLoading.setVisibility(View.GONE);
-                            shapeRipple.setRippleShape(new Image(bitmap));
-                            shapeRipple.setEnableSingleRipple(true);
-                            shapeRipple.setEnableRandomPosition(true);
-                            shapeRipple.setRippleMaximumRadius(200);
-                            shapeRipple.setRippleCount(2);
-                            shapeRipple.setRippleDuration(2000);
-                        }
-                    });
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-    }
-}
+//package com.rc.facecase.activity;
+//
+//import android.graphics.Bitmap;
+//import android.os.Bundle;
+//import android.support.v7.app.AppCompatActivity;
+//import android.view.View;
+//import android.view.Window;
+//import android.view.WindowManager;
+//import android.widget.ImageView;
+//
+//import com.bumptech.glide.Glide;
+//import com.bumptech.glide.request.target.SimpleTarget;
+//import com.bumptech.glide.request.transition.Transition;
+//import com.rc.facecase.R;
+//import com.rodolfonavalon.shaperipplelibrary.ShapeRipple;
+//import com.rodolfonavalon.shaperipplelibrary.model.Image;
+//
+//public class TestActivity extends AppCompatActivity {
+//
+//    private ImageView ivLoading;
+//    private ShapeRipple shapeRipple;
+//    private String imageUrl = "http://iexpresswholesale.com/faceoff-games/uploads/pictures/pele.jpg";
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//        setContentView(R.layout.activity_test);
+//
+//        ivLoading = (ImageView) findViewById(R.id.iv_loading);
+//        shapeRipple = (ShapeRipple) findViewById(R.id.shape_ripple);
+//        try {
+//            // Load
+//            Glide.with(TestActivity.this)
+//                    .asGif()
+//                    .load(R.drawable.gif_loading)
+//                    .into(ivLoading);
+//
+//            Glide
+//                    .with(TestActivity.this)
+//                    .asBitmap()
+//                    .load(imageUrl)
+//                    .into(new SimpleTarget<Bitmap>() {
+//                        @Override
+//                        public void onResourceReady(Bitmap bitmap, Transition<? super Bitmap> transition) {
+//                            //you can use loaded bitmap here
+//                            shapeRipple.setVisibility(View.VISIBLE);
+//                            ivLoading.setVisibility(View.GONE);
+//                            shapeRipple.setRippleShape(new Image(bitmap));
+//                            shapeRipple.setEnableSingleRipple(true);
+//                            shapeRipple.setEnableRandomPosition(true);
+//                            shapeRipple.setRippleMaximumRadius(200);
+//                            shapeRipple.setRippleCount(2);
+//                            shapeRipple.setRippleDuration(2000);
+//                        }
+//                    });
+//        } catch (Exception ex) {
+//            ex.printStackTrace();
+//        }
+//    }
+//}
